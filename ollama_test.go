fe29e1a65df62b8224a4fb1c0ac76a913bd5383b
Sugam Panthi <man359905@gmail.com>
2025-04-19 21:00:17 -0500

    #58 feat: refactor Ollama test functions to use deepseek.IsOllamaRunning and add image handling tests

--- ollama_test.go
+++ ollama_test.go
@@ -6,27 +6,14 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"net/http"
 	"testing"
-	"time"
 
 	"github.com/cohesion-org/deepseek-go"
 	"github.com/cohesion-org/deepseek-go/constants"
 )
 
-func isOllamaRunning() bool {
-	client := http.Client{
-		Timeout: 2 * time.Second,
-	}
-	resp, err := client.Get("http://localhost:11434/api/tags")
-	if err != nil {
-		return false
-	}
-	defer resp.Body.Close()
-	return resp.StatusCode == http.StatusOK
-}
 func TestChatCompletionWithOllama(t *testing.T) {
-	if !isOllamaRunning() {
+	if !deepseek.IsOllamaRunning() {
 		t.Skip("Ollama server is not running, skipping test")
 	}
 
@@ -48,7 +35,7 @@ func TestChatCompletionWithOllama(t *testing.T) {
 }
 
 func TestChatCompletionWithOllamaErrors(t *testing.T) {
-	if !isOllamaRunning() {
+	if !deepseek.IsOllamaRunning() {
 		t.Skip("Ollama server is not running, skipping test")
 	}
 
@@ -84,7 +71,7 @@ func TestChatCompletionWithOllamaErrors(t *testing.T) {
 
 // TestChatCompletionWithOllamaStream tests the streaming chat completion with Ollama
 func TestChatCompletionWithOllamaStream(t *testing.T) {
-	if !isOllamaRunning() {
+	if !deepseek.IsOllamaRunning() {
 		t.Skip("Ollama server is not running, skipping test")
 	}
 
@@ -100,6 +87,84 @@ func TestChatCompletionWithOllamaStream(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer res.Close()
+	fullMessage := ""
+
+	for {
+		response, err := res.Recv()
+		if errors.Is(err, io.EOF) {
+			fmt.Println("\nStream finished")
+			break
+		}
+		if err != nil {
+			fmt.Printf("\nStream error: %v\n", err)
+			break
+		}
+		for _, choice := range response.Choices {
+			fullMessage += choice.Delta.Content // Accumulate chunk content
+			log.Println(choice.Delta.Content)
+		}
+	}
+	if fullMessage == "" {
+		t.Fatal("Expected non-empty response")
+	}
+
+	t.Logf("The reponse is: %s", fullMessage)
+
+}
+
+// TestChatCompletionWithOllamaStreamWithImage tests the streaming chat completion with image
+func TestChatCompletionWithOllamaWithImage(t *testing.T) {
+	if !deepseek.IsOllamaRunning() {
+		t.Skip("Ollama server is not running, skipping test")
+	}
+	imgData, err := deepseek.ImageToBase64("internal/images/deepseek-go-big.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+	req := &deepseek.ChatCompletionRequestWithImage{
+		Model: "llava:latest",
+		Messages: []deepseek.ChatCompletionMessageWithImage{
+			deepseek.NewImageMessage("user", "What is this?", imgData),
+		},
+	}
+	fmt.Printf("Request: %v\n", req)
+
+	res, err := deepseek.CreateOllamaChatCompletionWithImage(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.Choices[0].Message.Content == "" {
+		t.Fatal("Expected non-empty response")
+	}
+
+	t.Logf("The reponse content is: %s", res.Choices[0].Message.Content)
+
+}
+
+func TestChatCompletionWithOllamaStreamWithImage(t *testing.T) {
+	if !deepseek.IsOllamaRunning() {
+		t.Skip("Ollama server is not running, skipping test")
+	}
+	imgData, err := deepseek.ImageToBase64("internal/images/deepseek-go-big.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+	req := &deepseek.StreamChatCompletionRequestWithImage{
+		Model: "llava:latest",
+		Messages: []deepseek.ChatCompletionMessageWithImage{
+			deepseek.NewImageMessage("user", "What is this?", imgData),
+		},
+	}
+	ctx := context.Background()
+	req.Stream = true
+	fmt.Printf("Request: %v\n", req)
+
+	res, err := deepseek.CreateOllamaChatCompletionStreamWithImage(ctx, req)
+	if err != nil {
+		t.Fatalf("The error is %s", err)
+	}
+
 	defer res.Close()
 	fullMessage := ""
 
